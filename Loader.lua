c0e45130aa942e6a91ebc028879b21ae3e6b4d19
ScriptInfinityHub <124055992+ScriptInfinityHub@users.noreply.github.com>
2023-02-26 22:54:56 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,35 @@
+local keycheck = false
+local username = nil
+local chrckName = game.Players.LocalPlayer.Name
+
+
+if _G.key == "GSJkJSD-HSHG1MNS-KJSJFKJH2" then
+   username = "LKAJHP"
+   keycheck = true
+end
+
+if keycheck == true then
+   if chrckName == username then
+       game.StarterGui:SetCore("SendNotification",{
+    Icon = "http://www.roblox.com/asset/?id=";
+    Title = "InfinityHub";
+    Text = "         Success!";
+    Duration = 5;
+})
+
+       else
+           game.StarterGui:SetCore("SendNotification",{
+    Icon = "http://www.roblox.com/asset/?id=501164554";
+    Title = "System Error";
+    Text = "You don't Have Name in the System";
+    Duration = 5;
+})
+   end
+   else
+    game.StarterGui:SetCore("SendNotification",{
+    Icon = "http://www.roblox.com/asset/?id=501164554";
+    Title = "System Error";
+    Text = "Key incorrect!";
+    Duration = 5;
+})
+end
